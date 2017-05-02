3bf4d5f644d404a6989e73397a7983285fe4b9bf
kinetik <kinetikm@gmail.com>
2017-05-02 10:50:46 +0300

    Fix all classes

--- homeworks/HW3/src/main/java/ru.atom/model/GameSession.java
+++ homeworks/HW3/src/main/java/ru.atom/model/GameSession.java
@@ -1,5 +1,7 @@
 package ru.atom.model;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import ru.atom.gameinterfaces.GameObject;
 import ru.atom.gameinterfaces.Temporary;
 import ru.atom.gameinterfaces.Tickable;
