4704bc4a8cf546839fb46a78c0e98bc1ec51261e
ilysko <ilya.lysko@gmail.com>
2017-05-02 23:37:14 +0300

    create Possess class

---
+++ homeworks/HW3/src/main/java/ru.atom/network/Possess.java
@@ -0,0 +1,32 @@
+package ru.atom.network;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+
+/**
+ * Created by ilysko on 02.05.17.
+ */
+public class Possess {
+    private final Topic topic;
+    private final int data;
+
+    public Possess(Topic topic, int data) {
+        this.topic = topic;
+        this.data = data;
+    }
+
+    @JsonCreator
+    public Possess(@JsonProperty("topic") Topic topic, @JsonProperty("data") JsonNode data) {
+        this.topic = topic;
+        this.data = (int) Integer.parseInt(data.toString());
+    }
+
+    public Topic getTopic() {
+        return topic;
+    }
+
+    public int getData() {
+        return data;
+    }
+}
